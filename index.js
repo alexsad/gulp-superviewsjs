6b323009ee5860de6d13758ab2fa1c75beb277ec
Alexandre Araujo <alexandre.araujo@mv.com.br>
2016-02-19 14:33:51 -0300

    gulp plugin to superviewjs

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var through = require('through-gulp');
+var StringDecoder = require('string_decoder').StringDecoder;
+var decoder = new StringDecoder('utf8');
+var superviewjs = require("superviews.js");
+
+
+var path = require('path');
+var gutil = require('gulp-util');
+var File = gutil.File;
+var PluginError = gutil.PluginError;
+
+var Concat = require('concat-with-sourcemaps');
+
+// file can be a vinyl file object or a string
+// when a string it will construct a new one
+module.exports = function(file, opt) {
+	if (!file) {
+		throw new PluginError('gulp-concat', 'Missing file option for gulp-concat');
+	}
+	opt = opt || {};
+
+	// to preserve existing |undefined| behaviour and to introduce |newLine: ""| for binaries
+	if (typeof opt.newLine !== 'string') {
+		opt.newLine = gutil.linefeed;
+	}
+	return through(function(file, encoding,callback) {
+		if (file.isNull()) {}
+		if (file.isBuffer()) {           
+			var templateFile = new Buffer(decoder.write(file.contents)); 
+			templateFile = superviewjs(decoder.write(templateFile));
+			file.contents = new Buffer(templateFile);
+		}
+		if (file.isStream()) {}
+		this.push(file);
+		callback();
+		}, function(callback) {
+		callback();
+	});   
+};
