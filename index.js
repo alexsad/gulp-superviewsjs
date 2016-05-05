71903a6f4bcffdbcb9b1368146fdf1e853588723
Derek Kent <dak@dak.org>
2016-05-04 23:31:28 -0400

    Remove gulp-util dependency

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-'use strict';
-
+var path = require('path');
 var through = require('through-gulp');
 var StringDecoder = require('string_decoder').StringDecoder;
 var decoder = new StringDecoder('utf8');
-var superviewjs = require("superviews.js");
-
-
-var path = require('path');
-var gutil = require('gulp-util');
-var File = gutil.File;
-var PluginError = gutil.PluginError;
+var superviews = require("superviews.js");
 
 // file can be a vinyl file object or a string
 // when a string it will construct a new one
@@ -17,13 +10,13 @@ module.exports = function(file, opt) {
 	opt = opt || {};
 	// to preserve existing |undefined| behaviour and to introduce |newLine: ""| for binaries
 	if (typeof opt.newLine !== 'string') {
-		opt.newLine = gutil.linefeed;
+		opt.newLine = '\n';
 	}
 	return through(function(file, encoding,callback) {
 		if (file.isNull()) {}
-		if (file.isBuffer()) {           
-			var templateFile = new Buffer(decoder.write(file.contents)); 
-			templateFile = superviewjs(decoder.write(templateFile).replace(/[\n\t\r]/g," "));
+		if (file.isBuffer()) {
+			var templateFile = new Buffer(decoder.write(file.contents));
+			templateFile = superviews(decoder.write(templateFile).replace(/[\n\t\r]/g," "));
 			file.contents = new Buffer(templateFile);
 		}
 		if (file.isStream()) {}
@@ -31,5 +24,5 @@ module.exports = function(file, opt) {
 		callback();
 		}, function(callback) {
 		callback();
-	});   
+	});
 };
