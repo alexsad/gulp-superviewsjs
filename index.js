f09218f80befe9ab4bd518ab4c3cb7d126b60421
Derek Kent <dak@dak.org>
2016-05-05 18:28:41 -0400

    Add support for es6 module option

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ var superviews = require("superviews.js");
 
 // file can be a vinyl file object or a string
 // when a string it will construct a new one
-module.exports = function(file, opt) {
+module.exports = function(opt) {
 	opt = opt || {};
 	// to preserve existing |undefined| behaviour and to introduce |newLine: ""| for binaries
 	if (typeof opt.newLine !== 'string') {
 		opt.newLine = '\n';
 	}
-	return through(function(file, encoding,callback) {
+	return through(function(file, encoding, callback) {
 		if (file.isNull()) {}
 		if (file.isBuffer()) {
 			var templateFile = new Buffer(decoder.write(file.contents));
-			templateFile = superviews(decoder.write(templateFile).replace(/[\n\t\r]/g," "));
+			templateFile = superviews(decoder.write(templateFile).replace(/[\n\t\r]/g," "), null, null, opt.es6);
 			file.contents = new Buffer(templateFile);
 		}
 		if (file.isStream()) {}
 		this.push(file);
 		callback();
-		}, function(callback) {
+	}, function(callback) {
 		callback();
 	});
 };
