06316497186fcc96a92d4880639d0d7d0ecb3ad7
alexsad <alex.query@gmail.com>
2016-07-16 13:57:07 +0000

    removed guambiarra

--- index.js
+++ index.js
@@ -19,14 +19,7 @@ module.exports = function(opt) {
 			var templateFile = new Buffer(decoder.write(file.contents));			
 			templateFile = superviews(
 										decoder.write(templateFile)
-										.replace(/(<\w+-\w+[^ ])(.+?>)/g,function($0,$1,$2){
-											var tmpRtn = $1+' props="{{attr_only_to_update_component:123456,'+$2.replace(/"/g,"'").replace(/>$/g,"").replace(/(=')/g,":'").replace(/" (\w)/g,"',($1)").replace(/'{(.+?)}'/g,'$1')+'}}">';
-											//console.log($1);
-											//console.log($2);
-											//console.log(tmpRtn);
-											return tmpRtn;
-										})
-										.replace(/<require from="([^"]+)"/g,"<delegating-import props=\"{{from:'$1'}}\"")
+										.replace(/<require from=/g,"<delegating-import from=")
 										.replace(/<\/require>/g,"</delegating-import>")										
 										.replace(/[\n\t\r]/g," ")
 										.replace(/ (\w*)\.((trigger)|(delegate))="([^"]+)"/g," on$1=\"{$event.preventDefault();$5}\"")
